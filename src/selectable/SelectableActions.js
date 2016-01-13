d21808f709f15ed65e940c92f5ebc132329caf8c
Alexandre Assouad <alexandre.assouad@gmail.com>
2016-01-13 09:03:35 +0000

    added slecteMultiple, deselectMultiple

--- src/selectable/SelectableActions.js
+++ src/selectable/SelectableActions.js
@@ -25,11 +25,27 @@ export let SelectableActions = ComposedActions => class extends ComposedActions 
     })
   }
 
+  selectMultiple(_records) {
+    this.dispatcher.dispatch({
+      namespace: this.constants.namespace,
+      type: this.constants.actions.selectMultiple,
+      records: _records
+    })
+  }
+
   deselect(_record) {
     this.dispatcher.dispatch({
       namespace: this.constants.namespace,
       type: this.constants.actions.deselect,
       record: _record
+    })
+  }
+
+  deselectMultiple(_records) {
+    this.dispatcher.dispatch({
+      namespace: this.constants.namespace,
+      type: this.constants.actions.deselectMultiple,
+      record: _records
     })
   }
 };
--- src/selectable/SelectableConstants.js
+++ src/selectable/SelectableConstants.js
@@ -2,7 +2,9 @@
 
 export let SelectableConstants = {
     "select":"select",
+    "selectMultiple":"select_multiple",
     "deselect":"deselect",
+    "deselectMultiple":"deselect_multiple",
     "selectAll":"select_all",
     "deselectAll":"deselect_all",
 }
