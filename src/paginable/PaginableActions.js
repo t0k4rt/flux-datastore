c063643bd18a507a262323afc40138d5a3e9578e
Alexandre Assouad <alexandre.assouad@gmail.com>
2016-01-28 14:54:08 +0000

    added paginable, trying to improve lib efficiency

--- src/paginable/PaginableActions.js
+++ src/paginable/PaginableActions.js
@@ -7,28 +7,28 @@ export let PaginableActions = ComposedActions => class extends ComposedActions {
       namespace: this.__constants.namespace,
       type: this.__constants.actions.next
     })
-  },
+  }
 
   prev(_context) {
     this.__dispatcher.dispatch({
       namespace: this.__constants.namespace,
       type: this.__constants.actions.prev
     })
-  },
+  }
 
   first(_context) {
     this.__dispatcher.dispatch({
       namespace: this.__constants.namespace,
       type: this.__constants.actions.first
     })
-  },
+  }
 
   last(_context) {
     this.__dispatcher.dispatch({
       namespace: this.__constants.namespace,
       type: this.__constants.actions.last
     })
-  },
+  }
 
   goto(_pageNumber, _context) {
     this.__dispatcher.dispatch({
