eeb04fc5352ec0223c34ace36179b3773b80e7d9
mfaule <faule@kairosfire.com>
2022-08-31 10:14:27 +0000

    Update sync check if a qs already exist in the route name

--- src/Sync.js
+++ src/Sync.js
@@ -22,7 +22,7 @@ class Sync {
       'delete': '${id}'
     };
 
-    if(_options.routes) {
+    if (_options.routes) {
       this.__routes = assign(this.__routes, _options.routes);
     }
   }
@@ -36,11 +36,11 @@ class Sync {
     let error = {};
     try {
       let errorResponse = JSON.parse(xhr.responseText);
-      error = {statusCode: xhr.status, message: errorResponse.message, title: errorResponse.title, raw: errorResponse};
-    } catch(e) {
-      error = {statusCode: xhr.status, message: "An unknown error occured"};
+      error = { statusCode: xhr.status, message: errorResponse.message, title: errorResponse.title, raw: errorResponse };
+    } catch (e) {
+      error = { statusCode: xhr.status, message: "An unknown error occured" };
     }
-    if(this.__debug) {
+    if (this.__debug) {
       console.error(error);
     }
     return error;
@@ -48,19 +48,19 @@ class Sync {
 
   http(_method, _url, _data) {
     //todo : add data;
-    let resolveFn = function(resolve, reject) {
+    let resolveFn = function (resolve, reject) {
       this.__jquery.ajax({
         url: _url,
         dataType: 'json',
         method: _method,
         cache: false
       })
-      .fail(function(xhr, textStatus, err) {
-        reject(this.__getErrorMessage(xhr));
-      }.bind(this))
-      .done(function(data) {
-        resolve(data);
-      });
+        .fail(function (xhr, textStatus, err) {
+          reject(this.__getErrorMessage(xhr));
+        }.bind(this))
+        .done(function (data) {
+          resolve(data);
+        });
     };
     return new Promise(resolveFn.bind(this));
   }
@@ -75,56 +75,56 @@ class Sync {
   fetch(id, queryParams = {}) {
     let _context = this.__context;
     this.__context = {};
-    let url = this.__generateUrl("fetch", assign({id: id},_context), queryParams);
+    let url = this.__generateUrl("fetch", assign({ id: id }, _context), queryParams);
     return this.http("GET", url);
   }
 
   create(record, queryParams = {}) {
     let _context = this.__context;
     this.__context = {};
-    let resolveFn = function(resolve, reject) {
+    let resolveFn = function (resolve, reject) {
       this.__jquery.ajax({
         url: this.__generateUrl('create', _context, queryParams),
         dataType: 'json',
         method: 'POST',
         data: record
       })
-      .fail(function(xhr, textStatus, err) {
-        reject(this.__getErrorMessage(xhr));
-      }.bind(this))
-      .done(function(_data) {
-        let data = _data;
-        // merge data from rest api
-        record = record.withMutations(function(_record) {
-          for(let prop in data) {
-            if(_record.has(prop) && data.hasOwnProperty(prop)) {
-              _record.set(prop, data[prop]);
+        .fail(function (xhr, textStatus, err) {
+          reject(this.__getErrorMessage(xhr));
+        }.bind(this))
+        .done(function (_data) {
+          let data = _data;
+          // merge data from rest api
+          record = record.withMutations(function (_record) {
+            for (let prop in data) {
+              if (_record.has(prop) && data.hasOwnProperty(prop)) {
+                _record.set(prop, data[prop]);
+              }
             }
-          }
+          });
+          resolve(record);
         });
-        resolve(record);
-      });
     };
     return new Promise(resolveFn.bind(this));
   }
 
-  update(record, queryParams = {}){
+  update(record, queryParams = {}) {
     let _context = this.__context;
     this.__context = {};
 
-    let resolveFn = function(resolve, reject) {
+    let resolveFn = function (resolve, reject) {
       this.__jquery.ajax({
-        url: this.__generateUrl('update', assign({id: record.get('id')},_context), queryParams),
+        url: this.__generateUrl('update', assign({ id: record.get('id') }, _context), queryParams),
         dataType: 'json',
         method: 'PUT',
         data: record
       })
-      .fail(function(xhr, textStatus, err) {
-        reject(this.__getErrorMessage(xhr));
-      }.bind(this))
-      .done(function() {
-        resolve(record);
-      });
+        .fail(function (xhr, textStatus, err) {
+          reject(this.__getErrorMessage(xhr));
+        }.bind(this))
+        .done(function () {
+          resolve(record);
+        });
     };
     return new Promise(resolveFn.bind(this));
   }
@@ -132,33 +132,33 @@ class Sync {
   delete(record, queryParams = {}) {
     let _context = this.__context;
     this.__context = {};
-    let _url = this.__generateUrl("delete", assign({id: record.get('id')}, _context), queryParams);
-    let resolveFn = function(resolve, reject) {
+    let _url = this.__generateUrl("delete", assign({ id: record.get('id') }, _context), queryParams);
+    let resolveFn = function (resolve, reject) {
       this.__jquery.ajax({
         url: _url,
         dataType: 'json',
         method: "DELETE",
         cache: false
       })
-      .fail(function(xhr, textStatus, err) {
-        reject(this.__getErrorMessage(xhr));
-      }.bind(this))
-      .done(function(data) {
-        resolve(record);
-      });
+        .fail(function (xhr, textStatus, err) {
+          reject(this.__getErrorMessage(xhr));
+        }.bind(this))
+        .done(function (data) {
+          resolve(record);
+        });
     };
     return new Promise(resolveFn.bind(this));
   }
 
-  __generateQueryString(params) {
+  __generateQueryString(params, alreadyContainsQS) {
     let qs = [];
-    for(let key in params) {
-      if(params.hasOwnProperty(key)) {
-        qs.push(key+"="+params[key]);
+    for (let key in params) {
+      if (params.hasOwnProperty(key)) {
+        qs.push(key + "=" + params[key]);
       }
     }
-    if(qs.length > 0) {
-      return "?"+qs.join("&");
+    if (qs.length > 0) {
+      return alreadyContainsQS ? "&" + qs.join("&") : "?" + qs.join("&");
     }
     else {
       return "";
@@ -167,7 +167,8 @@ class Sync {
 
   __generateUrl(_routeName, _routeParams = {}, _queryParams = {}) {
     let _compiled = template(this.__routes[_routeName]);
-    return this.__baseUrl + _compiled(_routeParams) + this.__generateQueryString(_queryParams);
+    let alreadyContainsQS = _compiled(_routeParams).indexOf("?") == -1 ? false : true;
+    return this.__baseUrl + _compiled(_routeParams) + this.__generateQueryString(_queryParams, alreadyContainsQS);
   }
 }
 
